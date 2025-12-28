3599b7f126963384efa990e75a15ab181c9e8d08
ishuh8484 <75384600+ishuh8484@users.noreply.github.com>
2025-12-28 22:44:05 +0530

    meaningful message

---
+++ Backend_Microservices/userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,40 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import com.fitness.userservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if (repository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already Exist");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+
+        User savedUser = repository.save(user);
+        UserResponse response = new UserResponse();
+        response.setId(savedUser.getId());
+        response.setPassword(savedUser.getPassword());
+        response.setEmail(savedUser.getEmail());
+        response.setFirstName(savedUser.getPassword());
+        response.setLastName(savedUser.getLastName());
+        response.setCreatedAt(savedUser.getCreatedAt());
+        response.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return response;
+    }
+}
