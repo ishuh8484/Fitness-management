d7326ff116bca1cd68b06a2bd41254e2681d9efd
ishuh8484 <75384600+ishuh8484@users.noreply.github.com>
2026-01-14 00:38:19 +0530

    Connect to gemini api , through ai service , where new gemeni service is created , and ai service class to generate recommendation , through passing activity in the form of key value mapping , to gemini , with proper prompt thus providing proper response

---
+++ Backend_Microservices/aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,47 @@
+package com.fitness.aiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+public class GeminiService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiURL;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getRecommendations(String details){
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[] {
+                            Map.of("text",details)
+                   })
+                }
+
+        );
+
+           String response = webClient.post()
+                   .uri(geminiApiURL)
+                   .header("Content-Type","application/json")
+                   .header("x-goog-api-key" ,geminiApiKey)
+                   .bodyValue(requestBody)
+                   .retrieve()
+                   .bodyToMono(String.class)
+                   .block();
+
+
+           return response;
+    }
+}
